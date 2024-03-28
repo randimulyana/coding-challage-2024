ac23195ac8936577ca54bfb02cc4edf36cb26c61
lolififteenftrz <randimulyanacampus@gmail.com>
2024-03-28 15:40:37 +0700

    day 1/356 coding challenge 2024

---
+++ 1-grow.js
@@ -0,0 +1,27 @@
+// Coding Challange 2024
+// 1/356 day
+// https://www.codewars.com/kata/57f780909f7e8e3183000078/solutions/javascript
+
+// Cara 1
+// function grow(x) {
+//   let result = x[0];
+//   for (let i = 1; i < x.length; i++) {
+//     result = result * x[i];
+//   }
+//   return result;
+// }
+
+// Cara 2
+// function grow(x) {
+//   const result = x.reduce((acc, curr) => acc * curr, 1);
+//   return result;
+// }
+// const grow = (x) => x.reduce((acc, curr) => acc * curr, 1);
+const grow = (x) => {
+  let res = 1;
+  for (let i = 0; i < x.length; i++) {
+    res *= x[i];
+  }
+  return res;
+};
+console.log(grow([1, 2, 3, 4]));
